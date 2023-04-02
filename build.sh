eeef6cf9220ee83055d2cfdaa01dc9e1dbe9345f
DarioMarret <javier_dario_marret@hotmail.com>
2023-04-02 16:59:55 -0500

    2023-04-02_16:59:55

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ git push
 export SHORT_COMMIT=$(git log -1 --pretty="%H" | cut -b -8)
 export DOCKER_IMAGE_VERSION="dev_${SHORT_COMMIT}"
 
+docker login -u "djmarret1992" -p "dckr_pat_hVke026bLagbYIPeknmTQEcK_7A"
+
 docker build -t djmarret1992/ticke-storange:${DOCKER_IMAGE_VERSION} -f Dockerfile .
-docker login -u "djmarret1992" -p "Tumadre1@" docker.io
 docker tag djmarret1992/ticke-storange:${DOCKER_IMAGE_VERSION} djmarret1992/ticke-storange:latest
 docker push djmarret1992/ticke-storange:${DOCKER_IMAGE_VERSION}
 docker push djmarret1992/ticke-storange:latest
--- src/public/wha.js
+++ src/public/wha.js
@@ -1,35 +1,24 @@
-const socket = io()
-const ContenQr = document.getElementById('contenQr')
-const img = document.getElementById('img')
-const instancia = document.getElementById('instancia')
+var DivImage = document.getElementById('images')
 
-
-window.onload = function(){
-    let valor = instancia.value
-    if(valor != null && valor.length != 0){
-        alert("text1");
-    }else{
-        // img.src = './img/whatsapp.png' 
-    }
-};
-
-instancia.addEventListener('change',function(){
-    console.log("change")
-//     ContenQr.innerHTML = `<div class="spinner-border text-success" role="status">
-//     <span class="visually-hidden">Loading...</span>
-//   </div>`
-  let data = instancia.value
-  socket.emit('change_intance',data)
+window.addEventListener('load', () => {
+    fetch('/api/get_image')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.archivos)
+            data.archivos.forEach(item => {
+                // if(true){
+                //     let domino = 'http://localhost:4000'+item.split('https://api.t-ickets.com/storange')[1]
+                //     DivImage.innerHTML += `
+                //             <img src="${domino}" alt="" width="250px">
+                //     `
+                // }
+                DivImage.innerHTML += `
+                <img src="${item}" alt="" width="250px">
+                `
+            })
+        })
 })
 
-socket.on('qr:',(data)=>{
-    console.log(data);
-    let valor = instancia.value
-    if(valor != null && valor.length != 0){
-        // ContenQr.innerHTML = ``
-        img.src = ``
-        img.src = './img/out.svg'
-    }else{
-        img.src = './img/whatsapp.png' 
-    }
-})
+
+
+
