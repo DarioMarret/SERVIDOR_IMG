a21f7c8dcfe0ffafd58606b02d99e89243e48492
DarioMarret <javier_dario_marret@hotmail.com>
2023-04-02 17:08:35 -0500

    2023-04-02_17:08:35

--- src/public/wha.js
+++ src/public/wha.js
@@ -1,23 +1,3 @@
-var DivImage = document.getElementById('images')
-
-window.addEventListener('load', () => {
-    fetch('https://api.t-ickets.com/storange/api/get_image')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.archivos)
-            data.archivos.forEach(item => {
-                // if(true){
-                //     let domino = 'http://localhost:4000'+item.split('https://api.t-ickets.com/storange')[1]
-                //     DivImage.innerHTML += `
-                //             <img src="${domino}" alt="" width="250px">
-                //     `
-                // }
-                DivImage.innerHTML += `
-                <img src="${item}" alt="" width="250px">
-                `
-            })
-        })
-})
 
 
 
