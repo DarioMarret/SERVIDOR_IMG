1db78193b03a9fe6db6a389fcd10e76536c88184
DarioMarret <javier_dario_marret@hotmail.com>
2023-04-24 11:36:20 -0500

    2023-04-24_11:36:20

--- src/server.js
+++ src/server.js
@@ -19,10 +19,10 @@ const port = process.env.PORT || 4000
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.json({
-    limit: '50mb',
-    extended: true
-}));
+// app.use(express.json({
+//     limit: '50mb',
+//     extended: true
+// }));
 
 
 // app.use(fileUpload({
